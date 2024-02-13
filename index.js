2f43e51710db22e377a9a61b8d5c6037cb0c4446
Arnab Jana <arnabjana0099@gmail.com>
2024-02-13 16:24:34 +0530

    modified

--- index.js
+++ index.js
@@ -23,74 +23,29 @@ class ApiError extends Error{
 app.get("/api",(req,res)=>res.send({message:"ok"}))
 
 app.post('/api/getdetails',async(req,res)=>{
-  try {
-    const { videoUrl } = req.body;
+  try{
+    const {videoUrl} = req.body
     if(!videoUrl) return new ApiError(400,"videoUrl is reqired")
 
-     const videoInfo = await ytdl.getInfo(videoUrl);
-     if(!videoInfo) return new ApiError (404,"video not found");
+    const videoInfo = await ytdl.getInfo(videoUrl);
+    if(!videoInfo) return new ApiError (404,"video not found");
 
-     const formats = videoInfo.formats.filter(item=>item.hasAudio&&item.hasVideo)
-     console.log({url:formats[0]})
-     return res.status(200).json({url:formats[0].url})
-  } catch (error) {
-    return new ApiError(400,error)
+    const audioFormats = ytdl.filterFormats(videoInfo.formats, 'audioonly');
+
+    if (audioFormats.length === 0) {
+      throw new Error('No audio-only formats available');
+    }
+
+    const audioFormat = ytdl.chooseFormat(audioFormats, { quality: 'highestaudio' })
+    const videoFormat = videoInfo.formats.filter((item)=>item.hasAudio==true&&item.hasVideo==true)
+    return res
+    .status(200)
+    .json({audioUrl:audioFormat.url,videoUrl:videoFormat[0].url})
+  }catch(err){
+    return new ApiError(400,err)
   }
 })
-app.post('/api/download', async (req, res) => {
-  console.log("started");
- try {
-     const { videoUrl,quality } = req.body;
-     if(!videoUrl || !quality) return new ApiError(400,"video url and video quality is requred")
-     
-     const videoInfo = await ytdl.getInfo(videoUrl);
-     if(!videoInfo) return new ApiError (404,"video not found");
 
-     const format = ytdl.chooseFormat(videoInfo.formats, quality);
-     if(!format) return new ApiError (404,"something went wrong");
-     console.log(format)
-    //  const videoStream = ytdl.downloadFromInfo(videoInfo, quality);
-    //  if(!videoStream) return new ApiError(500,"somthing went wrong while streaming video")
-
-     const fileName = `video-${Date.now()}.${format.container}`;
-     const filePath = `./downloads/${fileName}`;
-
-    //  videoStream.pipe(fs.createWriteStream(filePath));
-
-     videoStream.on('end', () => {
-       console.log("done in backend");
-       res.json({ downloadLink: `/api/download/${fileName}` });
-     })
- } catch (error) {
-    return new ApiError(400,error)
- };
-});
-app.get('/api/download/:fileName', (req, res) => {
-  const { fileName } = req.params;
-  const filePath = `./downloads/${fileName}`;
-  try {
-    res.download(filePath, fileName);
-    setTimeout(()=>{
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          console.error('Error deleting file:', err);
-        } else {
-          console.log('File deleted successfully');
-        }
-      })
-    },2000)
-  } catch (error) {
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting file:', err);
-      } else {
-        console.log('File deleted successfully');
-      }
-    })
-    console.log("new ApiError::",error)
-    return new ApiError(400,error)
-  }
-});
 
 app.listen(process.env.PORT || 6000, () => {
   console.log(`Server is running on port ${process.env.PORT || 6000}`);
